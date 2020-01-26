4491b671e0bfa7dfeea25102083e7337a6587f3d
Roc Zhang <roc_zhang@hotmail.com>
2020-01-26 11:48:28 -0800

    organize gallery

---
+++ random_name.py
@@ -0,0 +1,21 @@
+# Pythono3 code to rename multiple  
+# files in a directory or folder 
+  
+# importing os module 
+import os 
+import numpy as np
+
+path = '/Users/roc/Documents/02_personal-blog/Zhang-Haipeng.github.io/assets/img/gallery'
+
+# Function to rename multiple files 
+def main(): 
+
+    for filename in os.listdir(path):
+        if filename[-4:] == '.jpg': # pass other files
+            numb = str(np.random.randint(1,1000))
+            os.rename(path+'/'+filename, path+'/'+numb+'.jpg')
+            print(filename,'to',numb+'.jpg')
+    
+# Driver Code 
+if __name__ == '__main__': 
+    main() 
